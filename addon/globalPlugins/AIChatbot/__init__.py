82496ae1062cfe25f5d2c3dbbe0f113788827992
ferisofa203-rgb <ferisofa203@gmail.com>
2026-04-09 19:42:07 +0700

    latest version

--- addon/globalPlugins/AIChatbot/__init__.py
+++ addon/globalPlugins/AIChatbot/__init__.py
@@ -15,108 +15,112 @@ addonHandler.initTranslation()
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	def __init__(self):
+		super(GlobalPlugin, self).__init__()
+		initializeConfig()
+		self.chatDialog = None
+		self.chatAISettingsMenuItem = None
 
-    def __init__(self):
-        super(GlobalPlugin, self).__init__()
-        initializeConfig()
-        self.chatDialog = None
-        self.chatAISettingsMenuItem = None
-        wx.CallLater(30000, lambda: checkForUpdates(manual=False))
+		wx.CallLater(30000, lambda: checkForUpdates(manual=False))
 
-        # Register settings panel
-        try:
-            if AIChatSettingsPanel not in gui.settingsDialogs.NVDASettingsDialog.categoryClasses:
-                gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(AIChatSettingsPanel)
-        except Exception:
-            pass
+		# Register settings panel
+		try:
+			if AIChatSettingsPanel not in gui.settingsDialogs.NVDASettingsDialog.categoryClasses:
+				gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(AIChatSettingsPanel)
+		except Exception:
+			pass
 
-        # Add direct item to Preferences menu
-        try:
-            preferencesMenu = gui.mainFrame.sysTrayIcon.preferencesMenu
-            self.chatAISettingsMenuItem = preferencesMenu.Append(
-                wx.ID_ANY,
-                "Chat AI..."
-            )
-            gui.mainFrame.sysTrayIcon.Bind(
-                wx.EVT_MENU,
-                self.onOpenSettingsMenu,
-                self.chatAISettingsMenuItem
-            )
-        except Exception:
-            self.chatAISettingsMenuItem = None
+		# Add direct item to Preferences menu
+		try:
+			preferencesMenu = gui.mainFrame.sysTrayIcon.preferencesMenu
+			self.chatAISettingsMenuItem = preferencesMenu.Append(
+				wx.ID_ANY,
+				"Chat AI..."
+			)
+			gui.mainFrame.sysTrayIcon.Bind(
+				wx.EVT_MENU,
+				self.onOpenSettingsMenu,
+				self.chatAISettingsMenuItem
+			)
+		except Exception:
+			self.chatAISettingsMenuItem = None
 
-    def terminate(self):
-        try:
-            if self.chatDialog is not None:
-                self.chatDialog.Destroy()
-        except Exception:
-            pass
+	def terminate(self):
+		try:
+			if self.chatDialog is not None:
+				self.chatDialog.Destroy()
+		except Exception:
+			pass
+		self.chatDialog = None
 
-        self.chatDialog = None
+		# Remove Preferences menu item
+		try:
+			if self.chatAISettingsMenuItem is not None:
+				preferencesMenu = gui.mainFrame.sysTrayIcon.preferencesMenu
+				preferencesMenu.Remove(self.chatAISettingsMenuItem)
+				self.chatAISettingsMenuItem = None
+		except Exception:
+			pass
 
-        # Remove Preferences menu item
-        try:
-            if self.chatAISettingsMenuItem is not None:
-                preferencesMenu = gui.mainFrame.sysTrayIcon.preferencesMenu
-                preferencesMenu.Remove(self.chatAISettingsMenuItem)
-                self.chatAISettingsMenuItem = None
-        except Exception:
-            pass
+		# Remove settings panel
+		try:
+			if AIChatSettingsPanel in gui.settingsDialogs.NVDASettingsDialog.categoryClasses:
+				gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(AIChatSettingsPanel)
+		except Exception:
+			pass
 
-        # Remove settings panel
-        try:
-            if AIChatSettingsPanel in gui.settingsDialogs.NVDASettingsDialog.categoryClasses:
-                gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(AIChatSettingsPanel)
-        except Exception:
-            pass
+		super(GlobalPlugin, self).terminate()
 
-        super(GlobalPlugin, self).terminate()
+	@scriptHandler.script(
+		description="Open the AI chat dialog",
+		gesture="kb:NVDA+alt+a"
+	)
+	def script_openAIChat(self, gesture):
+		wx.CallAfter(self.openChatDialog)
 
-    @scriptHandler.script(
-        description="Open the AI chat dialog",
-        gesture="kb:NVDA+alt+a"
-    )
-    def script_openAIChat(self, gesture):
-        wx.CallAfter(self.openChatDialog)
+	@scriptHandler.script(
+		description="Open Chat AI settings",
+		gesture="kb:NVDA+alt+shift+a"
+	)
+	def script_openAIChatSettings(self, gesture):
+		wx.CallAfter(self.openSettingsDialog)
 
-    @scriptHandler.script(
-        description="Open Chat AI settings",
-        gesture="kb:NVDA+alt+shift+a"
-    )
-    def script_openAIChatSettings(self, gesture):
-        wx.CallAfter(self.openSettingsDialog)
+	def openChatDialog(self):
+		if self.chatDialog is not None:
+			try:
+				if self.chatDialog:
+					if not self.chatDialog.IsShown():
+						self.chatDialog.Show()
+					self.chatDialog.Raise()
+					self.chatDialog.SetFocus()
+					wx.CallLater(150, self.chatDialog.setInitialFocus)
+					return
+			except Exception:
+				self.chatDialog = None
 
-    def openChatDialog(self):
-        if self.chatDialog is not None:
-            try:
-                if self.chatDialog:
-                    self.chatDialog.Raise()
-                    self.chatDialog.SetFocus()
-                    return
-            except Exception:
-                self.chatDialog = None
+		self.chatDialog = ChatDialog(gui.mainFrame)
+		self.chatDialog.Bind(wx.EVT_CLOSE, self.onDialogClose)
+		self.chatDialog.Show()
+		self.chatDialog.Raise()
+		self.chatDialog.SetFocus()
+		wx.CallLater(150, self.chatDialog.setInitialFocus)
 
-        self.chatDialog = ChatDialog(gui.mainFrame)
-        self.chatDialog.Bind(wx.EVT_CLOSE, self.onDialogClose)
-        self.chatDialog.Show()
+	def openSettingsDialog(self):
+		try:
+			gui.mainFrame.popupSettingsDialog(
+				gui.settingsDialogs.NVDASettingsDialog,
+				AIChatSettingsPanel
+			)
+		except Exception:
+			pass
 
-    def openSettingsDialog(self):
-        try:
-            gui.mainFrame.popupSettingsDialog(
-                gui.settingsDialogs.NVDASettingsDialog,
-                AIChatSettingsPanel
-            )
-        except Exception:
-            pass
+	def onOpenSettingsMenu(self, event):
+		self.openSettingsDialog()
 
-    def onOpenSettingsMenu(self, event):
-        self.openSettingsDialog()
-
-    def onDialogClose(self, event):
-        try:
-            if self.chatDialog is not None:
-                self.chatDialog.Destroy()
-        except Exception:
-            pass
-
-        self.chatDialog = None
+	def onDialogClose(self, event):
+		try:
+			if self.chatDialog is not None:
+				self.chatDialog.Destroy()
+		except Exception:
+			pass
+		self.chatDialog = None
--- addon/globalPlugins/AIChatbot/dialogs.py
+++ addon/globalPlugins/AIChatbot/dialogs.py
@@ -2,7 +2,6 @@
 
 import wx
 import threading
-
 import ui
 
 from .client import sendChat, AIClientError
@@ -10,183 +9,185 @@ from .client import sendChat, AIClientError
 
 class ChatDialog(wx.Dialog):
 
-    def __init__(self, parent):
-        super(ChatDialog, self).__init__(
-            parent,
-            title="Chat AI",
-            size=(600, 500)
-        )
+	def __init__(self, parent):
+		super(ChatDialog, self).__init__(
+			parent,
+			title="Chat AI",
+			size=(600, 500)
+		)
 
-        self.chatHistory = []
-        self.lastResponse = ""
-        self.isBusy = False
+		self.chatHistory = []
+		self.lastResponse = ""
+		self.isBusy = False
 
-        self.buildUI()
-        self.Bind(wx.EVT_SHOW, self.onShow)
-        self.Centre()
-        wx.CallAfter(self.setInitialFocus)
+		self.buildUI()
+		self.Bind(wx.EVT_SHOW, self.onShow)
+		self.Bind(wx.EVT_CHAR_HOOK, self.onCharHook)
+		self.Centre()
 
-    def buildUI(self):
-        mainSizer = wx.BoxSizer(wx.VERTICAL)
+	def buildUI(self):
+		mainSizer = wx.BoxSizer(wx.VERTICAL)
 
-        historyLabel = wx.StaticText(self, label="Chat history:")
-        mainSizer.Add(historyLabel, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+		historyLabel = wx.StaticText(self, label="Chat history:")
+		mainSizer.Add(historyLabel, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
 
-        self.historyBox = wx.TextCtrl(
-            self,
-            style=wx.TE_MULTILINE | wx.TE_READONLY | wx.TE_RICH2
-        )
-        self.historyBox.SetName("Chat history")
-        mainSizer.Add(self.historyBox, 1, wx.EXPAND | wx.ALL, 5)
+		self.historyBox = wx.TextCtrl(
+			self,
+			style=wx.TE_MULTILINE | wx.TE_READONLY | wx.TE_RICH2
+		)
+		self.historyBox.SetName("Chat history")
+		mainSizer.Add(self.historyBox, 1, wx.EXPAND | wx.ALL, 5)
 
-        inputLabel = wx.StaticText(self, label="Your message:")
-        mainSizer.Add(inputLabel, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+		inputLabel = wx.StaticText(self, label="Your message:")
+		mainSizer.Add(inputLabel, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
 
-        self.inputBox = wx.TextCtrl(
-            self,
-            style=wx.TE_MULTILINE
-        )
-        self.inputBox.SetName("Your message")
-        mainSizer.Add(self.inputBox, 0, wx.EXPAND | wx.ALL, 5)
+		self.inputBox = wx.TextCtrl(
+			self,
+			style=wx.TE_MULTILINE
+		)
+		self.inputBox.SetName("Your message")
+		mainSizer.Add(self.inputBox, 0, wx.EXPAND | wx.ALL, 5)
 
-        buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
+		buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.sendButton = wx.Button(self, label="Send")
-        self.copyButton = wx.Button(self, label="Copy last reply")
-        self.clearButton = wx.Button(self, label="Clear chat")
-        self.closeButton = wx.Button(self, label="Close")
+		self.sendButton = wx.Button(self, label="Send")
+		self.copyButton = wx.Button(self, label="Copy last reply")
+		self.clearButton = wx.Button(self, label="Clear chat")
+		self.closeButton = wx.Button(self, label="Close")
 
-        buttonSizer.Add(self.sendButton, 0, wx.ALL, 5)
-        buttonSizer.Add(self.copyButton, 0, wx.ALL, 5)
-        buttonSizer.Add(self.clearButton, 0, wx.ALL, 5)
-        buttonSizer.Add(self.closeButton, 0, wx.ALL, 5)
+		buttonSizer.Add(self.sendButton, 0, wx.ALL, 5)
+		buttonSizer.Add(self.copyButton, 0, wx.ALL, 5)
+		buttonSizer.Add(self.clearButton, 0, wx.ALL, 5)
+		buttonSizer.Add(self.closeButton, 0, wx.ALL, 5)
 
-        mainSizer.Add(buttonSizer, 0, wx.CENTER)
+		mainSizer.Add(buttonSizer, 0, wx.CENTER)
 
-        self.SetSizer(mainSizer)
+		self.SetSizer(mainSizer)
 
-        self.sendButton.Bind(wx.EVT_BUTTON, self.onSend)
-        self.copyButton.Bind(wx.EVT_BUTTON, self.onCopy)
-        self.clearButton.Bind(wx.EVT_BUTTON, self.onClear)
-        self.closeButton.Bind(wx.EVT_BUTTON, self.onClose)
-        self.inputBox.Bind(wx.EVT_KEY_DOWN, self.onInputKeyDown)
+		self.sendButton.Bind(wx.EVT_BUTTON, self.onSend)
+		self.copyButton.Bind(wx.EVT_BUTTON, self.onCopy)
+		self.clearButton.Bind(wx.EVT_BUTTON, self.onClear)
+		self.closeButton.Bind(wx.EVT_BUTTON, self.onClose)
+		self.inputBox.Bind(wx.EVT_KEY_DOWN, self.onInputKeyDown)
 
-    def onShow(self, event):
-        if event.IsShown():
-            wx.CallAfter(self.setInitialFocus)
-        event.Skip()
+	def onShow(self, event):
+		if event.IsShown():
+			self.Raise()
+			wx.CallLater(150, self.setInitialFocus)
+		event.Skip()
 
-    def setInitialFocus(self):
-        self.inputBox.SetFocus()
-        self.inputBox.SetInsertionPointEnd()
+	def setInitialFocus(self):
+		try:
+			if self and self.IsShownOnScreen():
+				self.inputBox.SetFocus()
+				self.inputBox.SetInsertionPointEnd()
+		except Exception:
+			pass
 
-    def onInputKeyDown(self, event):
-        keyCode = event.GetKeyCode()
+	def onCharHook(self, event):
+		keyCode = event.GetKeyCode()
+		if keyCode == wx.WXK_ESCAPE:
+			self.Close()
+			return
+		event.Skip()
 
-        if keyCode in (wx.WXK_RETURN, wx.WXK_NUMPAD_ENTER):
-            if event.ShiftDown():
-                event.Skip()
-                return
+	def onInputKeyDown(self, event):
+		keyCode = event.GetKeyCode()
+		if keyCode in (wx.WXK_RETURN, wx.WXK_NUMPAD_ENTER):
+			if event.ShiftDown():
+				event.Skip()
+				return
+			self.onSend(None)
+			return
+		event.Skip()
 
-            self.onSend(None)
-            return
+	def onSend(self, event):
+		if self.isBusy:
+			ui.message("Please wait for the current response.")
+			return
 
-        event.Skip()
+		text = self.inputBox.GetValue().strip()
+		if not text:
+			ui.message("Message is empty.")
+			return
 
-    def onSend(self, event):
-        if self.isBusy:
-            ui.message("Please wait for the current response.")
-            return
+		self.chatHistory.append({
+			"role": "user",
+			"content": text
+		})
+		self.appendToHistory("You", text)
+		self.inputBox.SetValue("")
 
-        text = self.inputBox.GetValue().strip()
-        if not text:
-            ui.message("Message is empty.")
-            return
+		self.isBusy = True
+		self.sendButton.Disable()
+		ui.message("Sending request...")
 
-        self.chatHistory.append({
-            "role": "user",
-            "content": text
-        })
+		thread = threading.Thread(target=self.processRequest)
+		thread.daemon = True
+		thread.start()
 
-        self.appendToHistory("You", text)
-        self.inputBox.SetValue("")
+	def processRequest(self):
+		try:
+			reply = sendChat(self.chatHistory)
+		except AIClientError as e:
+			wx.CallAfter(self.onError, str(e))
+			return
 
-        self.isBusy = True
-        self.sendButton.Disable()
+		wx.CallAfter(self.onResponse, reply)
 
-        ui.message("Sending request...")
+	def onResponse(self, reply):
+		self.chatHistory.append({
+			"role": "assistant",
+			"content": reply
+		})
+		self.lastResponse = reply
+		self.appendToHistory("Asisten", reply)
 
-        thread = threading.Thread(target=self.processRequest)
-        thread.daemon = True
-        thread.start()
+		self.isBusy = False
+		self.sendButton.Enable(True)
+		self.historyBox.SetFocus()
 
-    def processRequest(self):
-        try:
-            reply = sendChat(self.chatHistory)
-        except AIClientError as e:
-            wx.CallAfter(self.onError, str(e))
-            return
+	def onError(self, message):
+		ui.message("Error: " + message)
+		self.isBusy = False
+		self.sendButton.Enable()
+		self.setInitialFocus()
 
-        wx.CallAfter(self.onResponse, reply)
+	def appendToHistory(self, speaker, text):
+		text = text.replace("\r\n", "\n").replace("\r", "\n")
+		lines = [line for line in text.split("\n") if line.strip() != ""]
+		cleanText = "\n".join(lines).strip()
 
-    def onResponse(self, reply):
-        self.chatHistory.append({
-            "role": "assistant",
-            "content": reply
-        })
+		entry = "%s: %s" % (speaker, cleanText)
+		current = self.historyBox.GetValue().replace("\r\n", "\n").replace("\r", "\n").rstrip("\n")
 
-        self.lastResponse = reply
-        self.appendToHistory("Asisten", reply)
+		if current:
+			newText = current + "\n" + entry
+		else:
+			newText = entry
 
-        self.isBusy = False
-        self.sendButton.Enable(True)
+		self.historyBox.SetValue(newText)
+		self.historyBox.SetInsertionPointEnd()
+		self.historyBox.ShowPosition(self.historyBox.GetLastPosition())
 
-        self.historyBox.SetFocus()
+	def onCopy(self, event):
+		if not self.lastResponse:
+			ui.message("No response to copy.")
+			return
 
-    def onError(self, message):
-        ui.message("Error: " + message)
+		if wx.TheClipboard.Open():
+			wx.TheClipboard.SetData(wx.TextDataObject(self.lastResponse))
+			wx.TheClipboard.Close()
+			ui.message("Response copied to clipboard.")
+		else:
+			ui.message("Failed to access clipboard.")
 
-        self.isBusy = False
-        self.sendButton.Enable()
-        self.setInitialFocus()
+	def onClear(self, event):
+		self.chatHistory = []
+		self.lastResponse = ""
+		self.historyBox.SetValue("")
+		ui.message("Chat cleared.")
+		self.setInitialFocus()
 
-    def appendToHistory(self, speaker, text):
-        text = text.replace("\r\n", "\n").replace("\r", "\n")
-
-        lines = [line for line in text.split("\n") if line.strip() != ""]
-        cleanText = "\n".join(lines).strip()
-
-        entry = "%s: %s" % (speaker, cleanText)
-
-        current = self.historyBox.GetValue().replace("\r\n", "\n").replace("\r", "\n").rstrip("\n")
-
-        if current:
-            newText = current + "\n" + entry
-        else:
-            newText = entry
-
-        self.historyBox.SetValue(newText)
-        self.historyBox.SetInsertionPointEnd()
-        self.historyBox.ShowPosition(self.historyBox.GetLastPosition())
-
-    def onCopy(self, event):
-        if not self.lastResponse:
-            ui.message("No response to copy.")
-            return
-
-        if wx.TheClipboard.Open():
-            wx.TheClipboard.SetData(wx.TextDataObject(self.lastResponse))
-            wx.TheClipboard.Close()
-            ui.message("Response copied to clipboard.")
-        else:
-            ui.message("Failed to access clipboard.")
-
-    def onClear(self, event):
-        self.chatHistory = []
-        self.lastResponse = ""
-        self.historyBox.SetValue("")
-        ui.message("Chat cleared.")
-        self.setInitialFocus()
-
-    def onClose(self, event):
-        self.Destroy()
+	def onClose(self, event):
+		self.Destroy()
